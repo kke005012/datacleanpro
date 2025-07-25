f1cae75747ab01d832ced3f1248bfef3bf5f6a9a
Kristi <kristi.esta@gmail.com>
2025-07-24 21:25:54 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -199,12 +199,14 @@ elif page == "Clean My Data":
                 )
                 st.stop()
             min_rows_required = 1
-            if len(df) < min_rows_required:
-                try:
-                    st.error("❌ This app supports a minimum of 1 row of data. Please upload a valid file.")
-                    st.stop()
-                except EmptyDataError:
-                    pass
+            try:
+               df = pd.read_csv(uploaded_file, header=0 if has_header else None)
+               if df.empty or len(df) < min_rows_required:
+                   st.error("❌ This app supports a minimum of 1 row of data. Please upload a valid file.")
+                   st.stop()
+            except pd.errors.EmptyDataError:
+                   st.error("❌ Uploaded file is empty or unreadable. Please try again.")
+                   st.stop()
             # ==== End Row Count Check ===                    
  
             st.session_state.raw_df = df.copy()
