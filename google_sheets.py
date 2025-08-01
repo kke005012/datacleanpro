c2351ebc171f74ed2c4006d8d2897f0ef17231d8
Kristi <kristi.esta@gmail.com>
2025-07-31 22:40:51 -0500

    Add files via upload

--- google_sheets.py
+++ google_sheets.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-
+import traceback
 
 
 def append_log_to_sheet(log_entry: dict, spreadsheet_id: str):
     try:
         credentials_dict = st.secrets["gcp_service_account"]
-        credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+        credentials = Credentials.from_service_account_info(credentials_dict)
 
         client = gspread.authorize(credentials)
         sheet = client.open_by_key(spreadsheet_id).sheet1
