e898ae94f9cf0c413aa35c172db16e590e6e0b2b
Kristi <kristi.esta@gmail.com>
2025-08-03 15:12:42 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -309,7 +309,7 @@ elif page == "Clean My Data":
             
             if cost > 0:
                 if st.button("💳 Pay Now"):
-                    checkout_url = create_checkout_session(cost, user_email, uploaded_file.name)
+                    checkout_url = create_checkout_session(cost, email, uploaded_file.name)
                     if checkout_url:
                         st.markdown(f"[🔗 Click here to complete payment]({checkout_url})", unsafe_allow_html=True)
             else:
