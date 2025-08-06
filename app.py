2c1495e8622294e72cd2203e8177f649e7838b1f
Kristi <kristi.esta@gmail.com>
2025-08-05 19:17:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -336,6 +336,7 @@ elif page == "Clean My Data":
                     "📥 Download Cleaned CSV",
                     data=cleaned_df.to_csv(index=False),
                     file_name=download_filename,
+                    key=download_free,
                     mime="text/csv"
                 )
                 log_entry = {
@@ -417,6 +418,7 @@ elif page == "Clean My Data":
                                 "📥 Download Cleaned CSV",
                                 data=cleaned_df.to_csv(index=False),
                                 file_name=download_filename,
+                                key=download_paid,
                                 mime="text/csv"
                             )
                             log_entry = {
@@ -445,6 +447,7 @@ elif page == "Clean My Data":
                         "📥 Download Cleaned CSV",
                         data=st.session_state["cleaned_csv"],
                         file_name=st.session_state["download_filename"],
+                        key=download_on_reset,
                         mime="text/csv"
                     )
 
